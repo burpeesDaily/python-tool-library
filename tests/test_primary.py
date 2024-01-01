55355817a53169c3e35cd13335ad2077006f3020
Shun Huang <shun@formosa1544.com>
2023-12-31 22:47:29 -0800

    Fix unit test errors.

--- tests/test_primary.py
+++ tests/test_primary.py
@@ -1,7 +1,10 @@
 """Unit tests for pytoolib.primary."""
+import pytest
+
 from pytoolib import primary
 
 
 def test_sample():
     """A place holder for unit tests."""
-    primary.list_diff([], [])
+    with pytest.raises(NotImplementedError):
+        primary.list_diff([], [])
